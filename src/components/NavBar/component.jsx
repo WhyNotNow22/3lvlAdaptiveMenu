ddda289ed8dfe761b5a077b638a5ae1f906fb2ef
Popov Mikhail <mishkinPopov@gmail.com>
2021-03-04 15:12:54 +0300

    almost

--- src/components/NavBar/component.jsx
+++ src/components/NavBar/component.jsx
@@ -5,13 +5,23 @@ function NavBar(props) {
     const { menuTabs } = props;
 
     const openMenu = (event) => {
-        event.target.closest('.container').classList.toggle("change"); 
-
+        event.target.closest('.container').classList.toggle("change");
+        if (event.target.closest('.container').classList.contains('change')) {
+            document.querySelector('.menu-list').style.display = 'block';
+        }
+        else {
+            document.querySelector('.menu-list').style.display = 'none';
+        }
+        const links = document.querySelectorAll('.link');
+        Array.from(links).map(elem => {
+          return elem.closest('.menu-link').lastElementChild.style.display = 'block';
+        })
+        
     }
     return (
         <div className='menu'>
             <ul className='menu-list'>
-                {Object.entries(menuTabs).map(([link, childrens], index) => <NavBarLink name={link} childrens={childrens} key={index}/>)}
+                {Object.entries(menuTabs).map(([link, childrens], index) => <NavBarLink name={link} childrens={childrens} key={index} />)}
             </ul>
             <div className="container" onClick={openMenu}>
                 <div className="bar1"></div>
--- src/components/NavBarLink/component.jsx
+++ src/components/NavBarLink/component.jsx
@@ -2,24 +2,54 @@ import './style.css'
 
 function NavBarLink(props) {
   const { name, childrens } = props;
+
+  const openSubLinks = (event) => {
+    if (event.target.innerHTML == 'ᐃ') {
+      event.target.closest('.menu-link').nextSibling.style.display = 'none';
+      event.target.innerHTML = 'ᐁ';
+    }
+    else {
+      event.target.closest('.menu-link').nextSibling.style.display = 'flex';
+      event.target.innerHTML = 'ᐃ';
+    }
+  }
+
   return (
-    <div className="menu-link">
-      <a className='main-link' href={name}>{name}</a>
-      <div className='drop-content'>
+    <>
+      <div className="menu-link">
+        <a className='main-link' href={name}>{name}</a>
+        <div className='drop-content'>
+          {
+            Object.entries(childrens).map(([link, subLink], index) => {
+              if (link) {
+                return (
+                  <ul className="drop-list" key={index}>
+                    <a className='link' href={link} key={index} >{link}</a>
+                    {Object.keys(subLink).map((link, index) => <li key={index}><a className='sub-link' href={link} key={index}>{link}</a></li>)}
+                  </ul>
+                )
+              }
+            })
+          }
+        </div>
+        <div className='blur' />
+        <div className='open-btn' onClick={openSubLinks}>ᐁ</div>
+      </div>
+      <div className='sub-menu'>
         {
           Object.entries(childrens).map(([link, subLink], index) => {
+            console.log(link)
             return (
-              <ul className="drop-list" key={index}>
-                <a className='link' href={link} key={index} >{link}</a>
-                {Object.keys(subLink).map((link, index) => <li key={index}><a className='sub-link' href={link} key={index}>{link}</a></li>)}
-              </ul>
+              <div className='sub-menu-link' key={index}>
+                <a href={link} >{link}</a>
+                <div className='open-btn' onClick={openSubLinks}>ᐁ</div>
+              </div>
             )
           })
         }
       </div>
-      <div className='blur' />
-    </div>
+    </>
   );
 }
-
+//ᐃ
 export default NavBarLink;
